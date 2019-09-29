21a6e52caffb2d412d344f850a0efff05f6dc2c6
Jannilol12 <j>
2019-09-29 18:48:21 +0200

    Events 2.0

--- src/nao/functions/events.java
+++ src/nao/functions/events.java
@@ -10,109 +10,68 @@ import java.util.concurrent.Executors;
 
 public class events {
     private static ExecutorService executor;
-    private static Thread footContact;
-    private static Thread speechRecognition;
-    private static Thread sonar;
+    private static long footContact;
+    private static long speechRecognition;
+    private static long sonar;
 
     static {
         executor = Executors.newFixedThreadPool(3);
     }
 
     public static void startFootContactChanged() {
-        if (footContact != null) {
-            return;
-        }
-        footContact = new Thread() {
-            @Override
-            public void run() {
-                while (!this.isInterrupted()) {
-                    try {
-                        currentApplication.getAlMemory().subscribeToEvent("footContact", new EventCallback<Float>() {
-                            @Override
-                            public void onEvent(Float variable) throws InterruptedException, CallError {
-                                if (variable == 1.0) {
-                                    System.out.println("FROM BOTTOM");
-                                }
-                            }
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        try {
+            footContact = currentApplication.getAlMemory().subscribeToEvent("footContact", new EventCallback<Float>() {
+                @Override
+                public void onEvent(Float variable) throws InterruptedException, CallError {
+                    if (variable == 1.0) {
+                        System.out.println("FROM BOTTOM");
                     }
                 }
-            }
-        };
-        footContact.start();
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void stopFootContactChanged(){
-        if(footContact == null){
-            return;
+        try {
+            currentApplication.getAlMemory().unsubscribeToEvent(footContact);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (CallError callError) {
+            callError.printStackTrace();
         }
-        footContact.interrupt();
-        footContact = null;
     }
 
 
     public static void startSpeechRecognition() {
         try {
             currentApplication.getAlSpeechRecognition().subscribe("Test");
+            speechRecognition =  currentApplication.getAlMemory().subscribeToEvent("WordRecognized", new EventCallback() {
+                @Override
+                public void onEvent(Object o) throws InterruptedException, CallError {
+                    System.out.println(o.toString());
+                    System.out.println(currentApplication.getAlMemory().getData("WordRecognized") + "" +  currentApplication.getAlMemory().getData("Test"));
+                }
+            });
         } catch (CallError callError) {
             callError.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-        if (speechRecognition != null) {
-            return;
-        }
-        speechRecognition = new Thread() {
-            @Override
-            public void run() {
-                while (!this.isInterrupted()) {
-                    try {
-                        System.out.println(currentApplication.getAlMemory().getData("WordRecognized") + "" +  currentApplication.getAlMemory().getData("Test"));
-
-                        currentApplication.getAlMemory().subscribeToEvent("WordRecognized", new EventCallback() {
-                            @Override
-                            public void onEvent(Object o) throws InterruptedException, CallError {
-                                System.out.println(o.toString());
-                            }
-                        });
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        speechRecognition.start();
     }
 
     public static void stopSpeechRecognition(){
         try {
             currentApplication.getAlSpeechRecognition().unsubscribe("Test");
+            currentApplication.getAlMemory().unsubscribeToEvent(speechRecognition);
         } catch (CallError callError) {
             callError.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        if(speechRecognition == null){
-            return;
-        }
-        speechRecognition.interrupt();
-        speechRecognition = null;
     }
 
     public static void startSonar(){
@@ -124,43 +83,30 @@ public class events {
         //
         //For example, if Value contains 0,40, Value1 1,2 and Value2 Max Detection range, the following values (3 to 9) will contain Max Detection range too.
         // It probably means you have the echo of the ground at 0,40m and another object at 1,2m. Left and Right sensors work the same way and allow you to locate objects.
-        if (sonar != null) {
-            return;
-        }
-        sonar = new Thread() {
-            @Override
-            public void run() {
-                while (!this.isInterrupted()) {
-                    try {
-                        String distanceLeft = currentApplication.getAlMemory().getData("Device/SubDeviceList/US/Left/Sensor/Value").toString();
-                        String distanceRight = currentApplication.getAlMemory().getData("Device/SubDeviceList/US/Right/Sensor/Value").toString();
 
-                        currentApplication.getAlMemory().subscribeToEvent("SonarLeftDetected", new EventCallback() {
-                            @Override
-                            public void onEvent(Object o) throws InterruptedException, CallError {
-                                System.out.println("Sonar Left Detected");
-                            }
-                        });
-                        System.out.println(distanceLeft + "|" + distanceRight);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        try {
+            sonar = currentApplication.getAlMemory().subscribeToEvent("SonarLeftDetected", new EventCallback() {
+                @Override
+                public void onEvent(Object o) throws InterruptedException, CallError {
+                    System.out.println("Sonar Left Detected");
+                    String distanceLeft = currentApplication.getAlMemory().getData("Device/SubDeviceList/US/Left/Sensor/Value").toString();
+                    String distanceRight = currentApplication.getAlMemory().getData("Device/SubDeviceList/US/Right/Sensor/Value").toString();
+                    System.out.println(distanceLeft + "|" + distanceRight);
                 }
-            }
-        };
-        sonar.start();
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void stopSonar(){
-        if(sonar == null){
-            return;
+        try {
+            currentApplication.getAlMemory().unsubscribeToEvent(sonar);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (CallError callError) {
+            callError.printStackTrace();
         }
-        sonar.interrupt();
-        sonar = null;
     }
 }
